53dbbad71cad5903a0b706328e60519cdb89e918
Robinson Presotto <robipresotto@gmail.com>
2017-02-04 11:59:20 +0100

    Creates the tank player controller class

---
+++ battleTank/Source/battleTank/Public/tankPlayerController.h
@@ -0,0 +1,19 @@
+// Copyright 2017 Robinson Presotto. All rights reserved.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "tankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API AtankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
