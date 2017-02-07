eaa22b21f5ff59ecce78f86cfd85a3f5da7fe387
Robinson Presotto <robipresotto@gmail.com>
2017-02-07 13:10:35 +0100

    Remove ever tick event for tank aiming component

--- battleTank/Source/battleTank/Private/tankAimingComponent.cpp
+++ battleTank/Source/battleTank/Private/tankAimingComponent.cpp
@@ -11,7 +11,7 @@ UtankAimingComponent::UtankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true; // TODO: Should this really tick?
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
