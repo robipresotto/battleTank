795a84301ba2f3565c263b52faccf6baafc6175c
Robinson Presotto <robipresotto@gmail.com>
2017-02-10 09:57:44 +0100

    Creates the tank force to throttle the tank

--- battleTank/Source/battleTank/Private/TankTrack.cpp
+++ battleTank/Source/battleTank/Private/TankTrack.cpp
@@ -8,4 +8,10 @@ void UTankTrack::SetThrottle(float throttle) {
     auto name = GetName();
     UE_LOG(LogTemp, Warning, TEXT("tank %s track throttle %f"), *name, throttle);
     
+    // TODO clamp actual throttle value so player can't  over-drive
+    auto forceApplied = GetForwardVector() * throttle * trackMaxDrivingForce;
+    auto forceLocation = GetComponentLocation();
+    auto tankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+    tankRoot->AddForceAtLocation(forceApplied, forceLocation);
+    
 }
--- battleTank/Source/battleTank/Public/TankTrack.h
+++ battleTank/Source/battleTank/Public/TankTrack.h
@@ -19,5 +19,8 @@ public:
     // Sets a throttle between -1 and +1
     UFUNCTION(BlueprintCallable, Category = Input)
     void SetThrottle(float throttle);
-	
+
+    // Max force per track in newtons (force = mass * acceleration) kg/s
+    UPROPERTY(EditDefaultsOnly)
+    float trackMaxDrivingForce = 400000; // assume 40 tonne tank and 1g accelleration
 };
